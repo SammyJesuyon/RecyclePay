892735a872ee71883709876750f012bfd932b1f4
Adenike Awofeso <adenikeawofeso@gmail.com>
2022-04-29 10:47:47 +0100

    changed db settings

--- project/config/settings.py
+++ project/config/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recyclepaydb',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
-DATABASES = {"default": dj_database_url.config(conn_max_age=60)}
+# DATABASES = {"default": dj_database_url.config(conn_max_age=60)}
 
 
 # Password validation
