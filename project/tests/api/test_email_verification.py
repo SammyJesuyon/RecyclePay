d4ad1f382c792399c35a85d134fad76da51fd5a7
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-05-19 11:39:44 +0100

    test changed to adapt to the changes made to the view

--- project/tests/api/test_email_verification.py
+++ project/tests/api/test_email_verification.py
@@ -2,28 +2,15 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
-from db.models import User
-from lib.utils import Util
 
 
 class TestEmailVerification(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.verify_email = reverse('verify_email')
-        self.email = 'dummy@yahoo.com'
-        self.password = '123456'
-        User.
-        
-    def test_verification_with_random_otp(self):
-        payload = 'dmljdG9yaWFraXRpZ29AeWFob28uY29t'
 
-        res = self.client.get(self.verify_email+payload)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        
-    # def test_verification_with_empty_fields(self):
-    #     payload = {
-    #         'email': '',
-    #
-    #     }
-    #     res = self.client.post(self.verify_email, payload)
-    #     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_verification_with_random_encoded_email(self):
+        encoded_email = 'ZG1samRHOXlhV0ZyYVhScFoyOUFlV0ZvYjI4dVkyOXQ='
+        res = self.client.get(self.verify_email+encoded_email)
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+
