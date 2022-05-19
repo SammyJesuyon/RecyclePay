d265e075ec58aa5ce634b36eaa5010192e7d58b0
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-05-19 15:56:56 +0100

    merge sorted

--- project/config/settings.py
+++ project/config/settings.py
@@ -126,7 +126,6 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-<<<<<<< HEAD
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
@@ -139,12 +138,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
-=======
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
->>>>>>> origin/main
 ]
 
 # Internationalization
