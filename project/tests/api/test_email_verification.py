c548c142922806cd911e1f5d0367d73f0dc27541
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-05-19 16:15:09 +0100

    more tests added

--- project/tests/api/test_email_verification.py
+++ project/tests/api/test_email_verification.py
@@ -19,3 +19,13 @@ class TestEmailVerification(TestCase):
     def test_verification(self):
         res = self.client.get(reverse(self.verify_email_url, kwargs={"encoded_email": self.encoded_email}))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_verification_with_unauthorized_email(self):
+        encoded_email = 'ZHVtZHVtQG1haWwuY29t'
+        res = self.client.get(reverse(self.verify_email_url, kwargs={"encoded_email": encoded_email}))
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_verification_with_empty_url(self):
+        encoded_email = ' '
+        res = self.client.get(reverse(self.verify_email_url, kwargs={"encoded_email": encoded_email}))
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
